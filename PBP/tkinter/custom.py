10aef5838544ffbda6563bca5073f10ce4629380
yanfd <2453317080@qq.com>
2025-02-17 00:02:13 +0800

    custom tkinter update

--- PBP/tkinter/custom.py
+++ PBP/tkinter/custom.py
@@ -1,9 +1,63 @@
 import customtkinter as ct
 
 app = ct.CTk()
-app.title("CustomTkinter")
+app.title("CustomTkinterAnimalRewrite")
 app.geometry("300x200")
 
+def update_label(stuff):
+    result_text.configure(text=f'You are a {stuff}')
+
 app._set_appearance_mode("dark")
-app.CTkLabel(text="which type of animals you are?").pack(padx=10, pady=10)
+label1 = ct.CTkLabel(app, text='which type of animals you are?').pack(padx=10, pady=10)
+options = ["select:","Dog", "Cat", "Bird", "Fish"]
+
+
+xiala = ct.CTkOptionMenu(app,values=options, command=update_label)
+xiala.pack(padx=10, pady=10)
+
+result_text = ct.CTkLabel(app, text="")
+result_text.pack(padx=10, pady=10)
+
+
+app.mainloop()
+
+
+
+
+
+
+
+# import tkinter as tk
+
+# root = tk.Tk()
+# root.title("tkinter testing")
+# root.geometry("300x200")
+
+# label = tk.Label(root, text="which type of animals you are?").pack(padx=10, pady=10)
+
+# options = [
+#     "Dog",
+#     "Cat",
+#     "Bird",
+#     "Fish"
+# ]
+
+# selected = tk.StringVar(value="请选择：")
+# xiala = tk.OptionMenu(root, selected, *options)
+# xiala.pack(padx=10, pady=10)
+
+# result_text = tk.Label(root, text="")
+# result_text.pack(padx=10, pady=10)
+
+
+# def update_label(*args):
+#     text = f'You are a {selected.get()}'
+#     result_text.config(text=text)
+
+# selected.trace_add("write", update_label)
+
+
+# if __name__ == "__main__":
+#     root.mainloop()
+
 
